3c7fd3468f8683ea773eea69f7ba3cf912ed7e88
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-15 08:04:27 -0500

    fix: Fix the override error

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        @SuppressWarnings({"Depracation",  })
         Author author = new Author("Sandi", "Metz");
 
         author.addBook("Practical Object-Oriented Design in Ruby");
